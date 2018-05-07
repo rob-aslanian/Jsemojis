6df566f2e7d08f01bfe2c6f07ac3ab7523139c4d
Robert <robert.aslaniani@gmail.com>
2018-05-07 21:24:54 +0400

    Js Library for adding emojis

---
+++ src/js/Emojis.js
@@ -0,0 +1,35 @@
+/**
+ * Creatd By @author Robert Aslanyan
+ * May 2018 
+ *  
+ */
+const EMOJIS = {
+    '_smile1':'&#x1F601;', '_smile2':'&#x1F602;' , '_smile3':'&#x1F603;' ,
+    '_smile4':'&#x1F604;', '_smile5':'&#x1F605;', '_smile6' : '&#x1F606;',
+    '_winking':'&#x1F609;','_tongue1':'&#x1F60B;','_tongue2':'&#x1F61D',
+    '_relived':'&#x1F60C;','_in_love':'&#x1F60D;','_smirking':'&#x1F60F;',
+    '_unamused':'&#x1F612;','_sweat':'&#x1F613;','_kiss':'&#x1F618;', 
+    '_kissing':'&#x1F61A;', '_winking_eye':'&#x1F61C;','_satisfied':'&#x1F60A;' , 
+    '_sad1':'&#x1F61E;' , '_sad2':'&#x1F614;', '_sad3':'&#x1F623;',
+    '_sad4':'&#x1F625;', '_sad5': '&#x1F630;' , '_sad6':'&#x1F629;', 
+    '_scary1':'&#x1F628;', '_scary2':'&#x1F631','_angry1':'&#x1F620;',
+    '_angry2':'&#x1F621;' , '_angry3':'&#x1F624;','_cry1':'&#x1F622;', 
+    '_cry2':'&#x1F62D;', '_sleepy':'&#x1F62A;','_tired':'&#x1F62B;',
+    '_shock1':'&#x1F632;' , '_shock2':'&#x1F633;', '_shock3':'&#x1F635;',
+    '_face_mask':'&#x1F637;' ,  '_cat_smile1':'&#x1F638', '_cat_smile2':'&#x1F639;',     
+    '_cat_smile3':'&#x1F63A;', '_cat_love':'&#x1F63B;','_cat_wry':'&#x1F63C;',                        
+    '_cat_kiss':'&#x1F63D;',  '_cat_angry':'&#x1F63E;', '_cat_cry':'&#x1F63F;',
+    '_cat_shock':'&#x1F640;', '_girl_face1':'&#x1F645;' , '_girl_face2':'&#x1F646;',
+    '_girl_face3':'&#x1F64B;' , '_girl_face4':'&#x1F64D;' , '_girl_face5':'&#x1F64E',
+    '_man1':'&#x1F647;','_mokey1':'&#x1F648;', '_monkey2':'&#x1F649;',
+    '_monkey_3':'&#x1F64A;', '_pray':'&#x1F64F;', '_peace':'&#x270C;',
+    '_raised1':'&#x1F64C;', '_raised2':'&#x270A;' , '_raised3':'&#x270B;',
+    '_clap':'&#x1F44F;', '_open_hand':'&#x1F450;','_like':'&#x1F44D;',
+    '_unlike':'&#x1F44E;','_ok':'&#x1F44C;', '_waving':'&#x1F44B;',
+    '_dam':'&#x1F44A;', '_to_right_hand':'&#x1F449;','_to_left_hand':'&#x1F448;',
+    '_love1':'&#x1F493;' , '_love2':'&#x1F495;' , '_love3':'&#x1F496;',
+    '_broken_heart':'&#x1F494;', '_poop':'&#x1F4A9', '_dash':'&#x1F4A8;',
+   
+}
+
+export default EMOJIS;
---
+++ src/js/Jsemoji.js
@@ -0,0 +1,229 @@
+import emojis from '../js/Emojis';
+
+const  CONFIG= {
+
+    el:'input[type="text"]',
+    insertTo:'afterend',
+
+    mainClass:'emoji_main',
+    emojiContentClass:'emojis_content',
+
+    btnClass:'emoji_btn',
+    btnText:':)',
+    btn:`span`
+}
+
+/** @private  */
+function _isNumber(value){
+    return typeof value == 'number';
+}
+
+/** @private  */
+function _isEqualTypes(value1, value2){
+    return typeof value1 === typeof value2;
+}
+
+/** @private  */
+function _isUndefined(value){
+    return value === undefined;
+}
+
+/** @private  */
+function _create(value , nameClass){
+
+     let el = document.createElement(value);
+
+     if(!_isUndefined(nameClass))
+        el.className = nameClass;
+
+    return el;
+}
+
+/** @private  Instead of  document.querySelector  */
+function _select(elem){
+    return document.querySelector(elem);
+}
+
+/** @private  Instead of  document.querySelectorAll*/
+function _selectAll(elem){
+    return document.querySelectorAll(elem);
+}
+
+const emojiParser = function(document){
+    let regExp = /_\w+/gi;
+  
+        console.log(document);
+        
+        for(let doc of document){
+            console.log(doc);
+            
+            let text = doc.textContent.trim();
+            let match = text.match(regExp);
+
+            if(regExp.test(text)){  
+                for(let code in emojis){
+                    for(let i = 0; i < match.length; i++){
+
+                        if(text.includes(match[i]) && code == match[i] ){
+                            text = text.replace(match[i] , emojis[code] );
+                            // doc.setAttribute('data-code' , code);
+                            doc.innerHTML = text;
+                        }     
+                    } 
+                } 
+            }
+
+        }
+}
+
+
+const JsEmoji = () =>  {
+
+    /** @protected Set Emojs */
+    const setEmojis = function(insert){
+
+        for(let code in emojis){
+            let span = _create('span' , 'emoji');
+            span.innerHTML = emojis[code];
+            span.setAttribute('title' , code);
+            insert.appendChild(span);
+        }  
+        
+        return getEmojis();
+        
+    }
+    /** @protected Set Emojs */
+    const getEmojis = function(){
+
+        let spans = _selectAll('.emoji'),
+                input = _select(CONFIG.el);
+                
+             spans.forEach(el => {
+                  el.addEventListener('click' ,  () => {
+                      let  text  = el.textContent;
+                        input.value += text + ' ';
+                  });
+            });
+
+    }
+
+    /** @protected Delete Emoji */
+    const deleteEmojis = function(){
+
+        let spans = _selectAll('.emoji');
+        if(!_isUndefined(spans))
+            spans.forEach(el => el.remove());
+
+    }
+
+    const emojiParser = function(document){
+        let regExp = /_\w+/gi;
+            console.log(_selectAll('li'));
+            
+            for(let doc of document){
+
+                let text = doc.textContent.trim();
+                let match = text.match(regExp);
+
+                if(regExp.test(text)){  
+                    for(let code in emojis){
+                        for(let i = 0; i < match.length; i++){
+
+                            if(text.includes(match[i]) && code == match[i] ){
+                                text = text.replace(match[i] , emojis[code] );
+                                // doc.setAttribute('data-code' , code);
+                                doc.innerHTML = text;
+                            }     
+                        } 
+                    } 
+                }
+
+            }
+    }
+
+
+    return {
+       set(obj){  
+			try{
+				let _confKeys = Object.keys(CONFIG),
+					 _objKeys  = Object.keys(obj);
+
+				_objKeys.map(key1 => {
+					_confKeys.map(key2 => {
+						try{
+                            
+							if(CONFIG.hasOwnProperty(key1)){    
+								if(_isEqualTypes(CONFIG[key1] , CONFIG[key2])){
+									let _obj = Object.create(null);
+									_obj[key1] = obj[key1];
+
+									Object.assign(CONFIG , _obj);
+								}
+								else 
+								  throw `Type of ${obj[key1]} not equls to CONIG type`
+							}
+							else
+								throw 'Inccorect object name';
+				
+						}
+						catch(e){
+							console.error(e);
+						}
+							
+					});
+				});
+			}
+			catch(e){
+				throw 'Error:Params Must be an object';
+			}
+        },
+        start(){
+            let insertAfter   = _select(CONFIG.el),
+                isClicked     = false;
+                    
+            if(insertAfter){
+
+               let mainDiv        =  _create('div' , CONFIG.mainClass),
+                   emojisConetent =  _create('div' , CONFIG.emojiContentClass),
+                   button         = _create(CONFIG.btn , CONFIG.btnClass);
+                   
+        
+               insertAfter.insertAdjacentElement(CONFIG.insertTo , mainDiv);
+
+               if(CONFIG.btnText !== '')
+                    button.textContent = CONFIG.btnText;
+
+               mainDiv.appendChild(button);
+               mainDiv.appendChild(emojisConetent);
+
+               button.addEventListener('click' , () => {
+
+                   isClicked = !isClicked;
+                   
+                   if(isClicked){
+                       emojisConetent.style = 'display:block;';
+
+                       let  emjisContentSelect = _select('.' + CONFIG.emojiContentClass);
+                       setEmojis(emjisContentSelect);
+                   }
+                   else{
+                     emojisConetent.style = 'display:none;';
+                     deleteEmojis();
+                   }
+
+               } , false);
+
+
+            }     
+           
+        }
+    }
+}
+
+window.JsEmoji = JsEmoji();
+
+export default JsEmoji;
+
+
+
+
